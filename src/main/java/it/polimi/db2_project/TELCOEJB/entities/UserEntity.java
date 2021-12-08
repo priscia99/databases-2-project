b8f2826b97354d5bb1ed76c6deafd5ce0aa63b93
Andrea Prisciantelli <priscia99@gmail.com>
2021-12-08 16:23:32 +0100

    Changed pom.xml

--- src/main/java/it/polimi/db2_project/TELCOEJB/entities/UserEntity.java
+++ src/main/java/it/polimi/db2_project/TELCOEJB/entities/UserEntity.java
@@ -1,14 +1,32 @@
 package it.polimi.db2_project.TELCOEJB.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+
+@NamedQueries({
+        @NamedQuery(name = "UserEntity.checkCredentials", query = "SELECT u FROM UserEntity u WHERE u.username = :username AND u.password = :password")
+})
 public class UserEntity {
-    int id;
-    String username;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    private String username;
+    private String password;
+    private boolean isInsolvent;
+
 
     public UserEntity(){}
 
-    public UserEntity(int id, String username){
+    public UserEntity(int id, String username, String password, boolean isInsolvent){
         this.id = id;
         this.username = username;
+        this.password = password;
+        this.isInsolvent = isInsolvent;
     }
 
     public int getId() {
