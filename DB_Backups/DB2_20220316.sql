80eda81e5dc02858df879e2eb4968eb2404f9507
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2022-03-16 16:59:15 +0100

    added bridge table

---
+++ DB_Backups/DB2_20220316.sql
@@ -0,0 +1,220 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db2
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alert`
+--
+
+DROP TABLE IF EXISTS `alert`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alert` (
+  `alertId` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) NOT NULL,
+  `amount` float NOT NULL,
+  `lastRejectionDateTime` timestamp NOT NULL,
+  PRIMARY KEY (`alertId`),
+  KEY `username_idx` (`username`),
+  CONSTRAINT `username` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alert`
+--
+
+LOCK TABLES `alert` WRITE;
+/*!40000 ALTER TABLE `alert` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alert` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `optionalproduct`
+--
+
+DROP TABLE IF EXISTS `optionalproduct`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `optionalproduct` (
+  `productID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `monthlyFee` float NOT NULL,
+  PRIMARY KEY (`productID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `optionalproduct`
+--
+
+LOCK TABLES `optionalproduct` WRITE;
+/*!40000 ALTER TABLE `optionalproduct` DISABLE KEYS */;
+/*!40000 ALTER TABLE `optionalproduct` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order` (
+  `orderId` int NOT NULL AUTO_INCREMENT,
+  `creationDateTime` timestamp NOT NULL,
+  `totalFee` float NOT NULL,
+  `startTime` timestamp NOT NULL,
+  `endTime` timestamp NOT NULL,
+  `orderState` char(20) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `packageId` int NOT NULL,
+  `validityPeriod` int NOT NULL,
+  `monthlyFee` float NOT NULL,
+  PRIMARY KEY (`orderId`),
+  KEY `packageId_idx` (`packageId`,`validityPeriod`,`monthlyFee`),
+  CONSTRAINT `servicePackage` FOREIGN KEY (`packageId`, `validityPeriod`, `monthlyFee`) REFERENCES `servicepackage` (`packageId`, `validityPeriod`, `monthlyFee`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order`
+--
+
+LOCK TABLES `order` WRITE;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `service`
+--
+
+DROP TABLE IF EXISTS `service`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `service` (
+  `serviceId` int NOT NULL AUTO_INCREMENT,
+  `serviceType` char(20) NOT NULL,
+  `includedMinutes` int DEFAULT NULL,
+  `includedSms` int DEFAULT NULL,
+  `feeMinutes` float DEFAULT NULL,
+  `feeSms` float DEFAULT NULL,
+  `includedGb` int DEFAULT NULL,
+  `feeGb` float DEFAULT NULL,
+  PRIMARY KEY (`serviceId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `service`
+--
+
+LOCK TABLES `service` WRITE;
+/*!40000 ALTER TABLE `service` DISABLE KEYS */;
+INSERT INTO `service` VALUES (1,'MOBILE_PHONE',3,5,7,10,0,0),(2,'MOBILE_INTERNET',0,0,0,0,20,1);
+/*!40000 ALTER TABLE `service` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicepackage`
+--
+
+DROP TABLE IF EXISTS `servicepackage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servicepackage` (
+  `packageId` int NOT NULL AUTO_INCREMENT,
+  `validityPeriod` int NOT NULL,
+  `monthlyFee` float NOT NULL,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`packageId`,`validityPeriod`,`monthlyFee`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicepackage`
+--
+
+LOCK TABLES `servicepackage` WRITE;
+/*!40000 ALTER TABLE `servicepackage` DISABLE KEYS */;
+INSERT INTO `servicepackage` VALUES (1,12,20,'prova');
+/*!40000 ALTER TABLE `servicepackage` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicepackage_service`
+--
+
+DROP TABLE IF EXISTS `servicepackage_service`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servicepackage_service` (
+  `servicepackage_id` int NOT NULL,
+  `service_id` int NOT NULL,
+  UNIQUE KEY `servicepackage_service_unique` (`servicepackage_id`,`service_id`),
+  KEY `servicepackage_service_service` (`service_id`),
+  CONSTRAINT `servicepackage_service_service` FOREIGN KEY (`service_id`) REFERENCES `service` (`serviceId`),
+  CONSTRAINT `servicepackage_service_servicepackage` FOREIGN KEY (`servicepackage_id`) REFERENCES `servicepackage` (`packageId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicepackage_service`
+--
+
+LOCK TABLES `servicepackage_service` WRITE;
+/*!40000 ALTER TABLE `servicepackage_service` DISABLE KEYS */;
+INSERT INTO `servicepackage_service` VALUES (1,1),(1,2);
+/*!40000 ALTER TABLE `servicepackage_service` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `username` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `isInsolvent` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('andrea','1234','',0),('gianluca','1234','gianluca@gianluca.it',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-16 16:44:06
