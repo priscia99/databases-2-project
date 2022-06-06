8f2938b77970d00bea2a2083cbe0f474288d41ba
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2022-06-06 09:55:45 +0200

    FI

--- src/main/java/it/polimi/db2_project/TELCOEJB/entities/UserEntity.java
+++ src/main/java/it/polimi/db2_project/TELCOEJB/entities/UserEntity.java
@@ -7,8 +7,8 @@ import java.util.List;
 @Table(name = "users")
 
 @NamedQueries({
-        @NamedQuery(name = "UserEntity.checkCredentials", query = "SELECT u FROM UserEntity u WHERE u.username = :username AND u.password = :password"),
-        @NamedQuery(name = "UserEntity.findByEmail", query = "SELECT u FROM UserEntity u WHERE u.email = :email")
+        @NamedQuery(name = "UserEntity.checkCredentials", query = "SELECT DISTINCT u FROM UserEntity u WHERE u.username = :username AND u.password = :password"),
+        @NamedQuery(name = "UserEntity.findByEmail", query = "SELECT DISTINCT u FROM UserEntity u WHERE u.email = :email")
 })
 public class UserEntity {
 
